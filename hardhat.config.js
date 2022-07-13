859e0d9fd9f7b58f0b1aec6ac44daacd0479dce6
Michael Hofweller <michael.hofweller@gmail.com>
2022-07-13 10:28:56 -0700

    etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,7 @@ module.exports = {
                         url: process.env.PROD_ALCHEMY_KEY,
                         accounts: [process.env.PRIVATE_KEY],
                 },
+                etherscan: {
+                  apiKey: process.env.ETHERSCAN_KEY
         },
 };
